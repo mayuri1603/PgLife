cb18996ab637ab9825464585a0d1cfe67926bad9
mayuri1603 <mrparaskar@gmail.com>
2022-12-28 14:37:36 -1200

    PGLife_Project

---
+++ react-app/src/FilterBar_css.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { base_path } from './utils.js';
+
+const FilterBar = () => {
+  return (
+    <div class="filter-bar row justify-content-around">
+            <div class="col-auto" data-toggle="modal" data-target="#filter-modal">
+                <img src="img/filter.png" alt="filter" />
+                <span>Filter</span>
+            </div>
+            <div class="col-auto">
+                <img src="img/desc.png" alt="sort-desc" />
+                <span>Highest rent first</span>
+            </div>
+            <div class="col-auto">
+                <img src="img/asc.png" alt="sort-asc" />
+                <span>Lowest rent first</span>
+            </div>
+        </div>
+  );
+}
+
+export default FilterBar;
